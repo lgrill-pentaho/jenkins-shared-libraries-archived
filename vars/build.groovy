13cf5cf20d9d6af9124644396cbf181af9db4e1f
Larry Grill <larry.grill@hitachivantara.com>
2018-01-04 14:09:36 -0500

    no println in the shared lib

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,12 +1,9 @@
 import org.hvbuilders.*
 
 def call(buildDataFilePath) {
-  println "Loading build data from ${buildDataFilePath}"
+  echo "Loading build data from ${buildDataFilePath}"
   def loader = new LoadBuildData()
   def data = loader.LoadBuildControlData(buildDataFilePath)
-  data.each{
-    println it
-  }
 
   return data
 }
